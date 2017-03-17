28183ad97d2cc7c658b348f127f6dddc241d865b
Anne Zhou <anne.zhou920@gmail.com>
2017-03-17 11:06:34 -0400

    pushed json file to firebase

--- db/firebase.js
+++ db/firebase.js
@@ -1,11 +1,12 @@
 import firebaseKeys from '../machikey'
 import firebase from 'firebase'
+import machiObject from '../public/machiObject.json'
 
 firebase.initializeApp(firebaseKeys)
 
-// firebase.database().ref('hello').set('bye')
+// const database = firebase.database().ref('hello').set('bye')
 // firebase.datbase()
-const database = firebase.database()
+const database = firebase.database().ref('/').set(machiObject)
 const auth = firebase.auth()
 
 // export default firebase
