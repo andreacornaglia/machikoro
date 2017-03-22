e8c44a1259d20a39411b187838d9263609b849a9
Andrea Cornaglia <andreacornaglia@gmail.com>
2017-03-22 17:15:01 -0400

    tests, integrating to rolldice

--- app/cards/cards.js
+++ app/cards/cards.js
@@ -196,7 +196,11 @@ export const businessCenter = {
   cost: 8,
   industry: 'antenna',
   cardDescription: "Trade one non [antenna icon] establishment with another player, on your turn only",
-  imgURL: '/images/subway.png'
+  imgURL: '/images/subway.png',
+  //placeholder fn, update in the future
+  cardFn: function(currentPlayer, gameState) {
+    return {money:0}
+  }
 };
 
 export const stadium = {
@@ -247,7 +251,11 @@ export const tvStation = {
   cost: 7,
   industry: 'antenna',
   cardDescription: "Take 5 coins from any one player, on your turn only",
-  imgURL: '/images/radio-tower.png'
+  imgURL: '/images/radio-tower.png',
+  //placeholder fn, update in the future
+  cardFn: function(currentPlayer, gameState) {
+    return {money:0}
+  }
 };
 
 export const powerPlant = {
@@ -301,7 +309,7 @@ export const theatre = {
   cardDescription: "Get 5 coins from the bank, on anyone's turn",
   imgURL: '/images/subway.png',
   cardFn: function(currentPlayer, gameState){
-    if (this.diceValue = gameState.diceValue) {
+    if (this.diceValue === gameState.diceValue) {
       const currentPlayerObj = gameState.players[currentPlayer];
       const numCards = currentPlayerObj.cards.theatre;
       const gainedAmount = numCards * 5;
