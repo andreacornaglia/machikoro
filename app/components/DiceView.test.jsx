7456a884ca345e0b7398b9a2304de555db2f1576
Anne Zhou <anne.zhou920@gmail.com>
2017-03-23 11:47:54 -0400

    changed dice test

--- app/components/DiceView.test.jsx
+++ app/components/DiceView.test.jsx
@@ -1,26 +1,38 @@
+import React from 'react'
 import chai, {expect} from 'chai'
-// import * as cardFunctions from './cards';
-import {rollDice} from './DiceView'
+chai.use(require('chai-enzyme')())
+import {shallow} from 'enzyme'
+import {spy} from 'sinon'
+chai.use(require('sinon-chai'))
 
-describe('Roll Dice function', () => {
+import DiceView, {rollDice} from './DiceView'
+
+describe('<DiceView />', () => {
+  // let root
+  // beforeEach('render the root', () =>
+  //   root = shallow(<DiceView />)
+  // )
+  //
+  // it('has a rollDice button', () => {
+  //   const roll = root.find('input[type="submit"]')
+  //   expect(roll).to.have.length(1)
+  // })
+
+  describe('roll dice function', () => {
 
     it('is a function', () => {
        expect(rollDice).to.be.an('function');
    });
 
-    describe('Roll dice functionality', () => {
-
-        it('should return a number from 1 - 6 when one player rolls one dice', () => {
-
-            const result = rollDice(1)
-            expect(result).to.be.within(1,6);
-        });
-
-        it('should return a number from 1 - 12 when one player rolls dice dice', () => {
-
-            const result = rollDice(2)
-            expect(result).to.be.within(1,12);
-        });
+    it('should return a number from 1 - 6 when one player rolls one dice', () => {
+        const result = rollDice(1)
+        expect(result).to.be.within(1,6);
     });
 
-});
+    it('should return a number from 1 - 12 when one player rolls dice dice', () => {
+        const result = rollDice(2)
+        expect(result).to.be.within(1,12);
+    });
+
+  })
+})
