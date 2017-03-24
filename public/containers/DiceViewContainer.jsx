6112f70bfe2c10734798d283e8d2ea80e96f3957
Susan Flanagan <SusanFlanagan7@gmail.com>
2017-03-24 00:53:47 -0400

    revised businessCenterFn

---
+++ public/containers/DiceViewContainer.jsx
@@ -0,0 +1,19 @@
+// import { connect } from 'react-redux';
+// import DiceView from '../components/DiceView.jsx'
+// import {getNewDiceVal} from '../reducers/dice'
+//
+// const mapStateToProps = (state) => {
+//   return {
+//     game: state.game
+//   }
+// }
+//
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     onDiceRoll: (diceVal) => dispatch(getNewDiceVal(diceVal))
+//   }
+// }
+//
+// const DiceViewContainer = connect(mapStateToProps, mapDispatchToProps)(DiceView);
+//
+// export default DiceViewContainer;
